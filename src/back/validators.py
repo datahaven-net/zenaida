d49123746619e622b6d8fdf8f063d2f8a6949229
Ilhan Mercan <ilhanmercan@hotmail.com>
2019-02-08 20:11:07 +0100

    NEW: Add validators for Profile and Contact Models

---
+++ src/back/validators.py
@@ -0,0 +1,20 @@
+from django.core.validators import RegexValidator
+from django.db import models
+
+from back.constants import COUNTRIES
+
+phone_regex = RegexValidator(
+    regex=r'^\+?1?\d{9,15}$',
+    message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
+)
+
+
+class CountryField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('max_length', 2)
+        kwargs.setdefault('choices', COUNTRIES)
+
+        super(CountryField, self).__init__(*args, **kwargs)
+
+    def get_internal_type(self):
+        return "CharField"

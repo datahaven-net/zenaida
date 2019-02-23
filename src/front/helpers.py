bd481ab192d982e858adf69bd10fabe3e8844793
Ilhan Mercan <ilhanmercan@hotmail.com>
2019-02-23 21:47:02 +0100

    NEW: Add a method that generates transfer code for domain transfers

---
+++ src/front/helpers.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+
+def get_transfer_code():
+    chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
+    size = random.randint(8, 12)
+    return ''.join(random.choice(chars) for x in range(size))

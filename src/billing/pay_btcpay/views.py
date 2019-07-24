752dd02b1405ea67a8ae3a893c7bd35b5f5409ea
Veselin Penev <penev.veselin@gmail.com>
2019-07-24 13:24:22 +0300

    extending BTCPay invoice info with customer details

--- src/billing/pay_btcpay/views.py
+++ src/billing/pay_btcpay/views.py
@@ -14,23 +14,44 @@ logger = logging.getLogger(__name__)
 
 
 class ProcessPaymentView(LoginRequiredMixin, View):
+
     def post(self, request, *args, **kwargs):
         transaction_id = kwargs.get('transaction_id')
         amount = request.POST['amount']
         try:
             client = btcpay.BTCPayClient(
-                    host=settings.ZENAIDA_BTCPAY_HOST,
-                    pem=settings.ZENAIDA_BTCPAY_CLIENT_PRIVATE_KEY,
-                    tokens={"merchant": settings.ZENAIDA_BTCPAY_MERCHANT}
-                )
+                host=settings.ZENAIDA_BTCPAY_HOST,
+                pem=settings.ZENAIDA_BTCPAY_CLIENT_PRIVATE_KEY,
+                tokens={"merchant": settings.ZENAIDA_BTCPAY_MERCHANT}
+            )
 
-            btcpay_invoice = client.create_invoice({"price": amount, "currency": "USD"})
+            btcpay_invoice = client.create_invoice(
+                payload={
+                    "price": amount,
+                    "currency": "USD",
+                    "orderId": transaction_id,
+                    "notificationEmail": request.user.email,
+                    "physical": False,
+                    "itemDesc": "BitCoin payment on %s" % settings.SITE_BASE_URL,
+                    "buyer": {
+                        "name": request.user.profile.person_name,
+                        "address1": request.user.profile.address_street,
+                        "locality": request.user.profile.address_city,
+                        "region": request.user.profile.address_province,
+                        "postalCode": request.user.profile.address_postal_code,
+                        "country": request.user.profile.address_country,
+                        "phone": request.user.profile.contact_voice,
+                        "notify": True,
+                        "email": request.user.email,
+                    },
+                },
+            )
 
             BTCPayInvoice.invoices.create(
                 transaction_id=transaction_id,
                 invoice_id=btcpay_invoice.get('id'),
                 status=btcpay_invoice.get('status'),
-                amount=amount
+                amount=amount,
             )
         except Exception as exc:
             logger.exception(exc)

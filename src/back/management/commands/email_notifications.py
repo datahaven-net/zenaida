9f2af0be1e6b06ecbc60d300bd973f7ce8933fd5
Veselin Penev <penev.veselin@gmail.com>
2019-10-15 23:05:32 +0200

    added placeholder for email notifications job

---
+++ src/back/management/commands/email_notifications.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+
+    help = 'Sending Email notifications to customers'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--dry_run', action='store_true', dest='dry_run')
+
+    def handle(self, dry_run, *args, **options):
+        started = time.time()
+
+        # TODO: to be implemented
+
+        self.stdout.write(self.style.SUCCESS('Done in %.3f seconds' % (time.time() - started)))

837c1118658a477a6d574d3581d3c22a2c514182
Veselin Penev <penev.veselin@gmail.com>
2023-04-30 21:06:37 +0200

    added requests logging into database

---
+++ src/logs/admin.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy
+
+from logs.models import RequestLog
+
+
+class HasExceptionListFilter(admin.SimpleListFilter):
+
+    title = gettext_lazy('have exception')
+    parameter_name = 'has_exception'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('yes', gettext_lazy('Has exception')),
+            ('no', gettext_lazy("Doesn't have exception")),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'yes':
+            return queryset.filter(exception__isnull=False)
+        if self.value() == 'no':
+            return queryset.filter(exception__isnull=True)
+
+
+class RequestLogAdmin(admin.ModelAdmin):
+    list_display = ('timestamp', 'ip_address', 'user', 'method', 'path', 'status_code', 'duration', 'no_exception')
+    list_filter = ('timestamp', 'ip_address', 'user', 'method', 'path', 'status_code', HasExceptionListFilter)
+    search_fields = ('timestamp', 'ip_address', 'user', 'method', 'path', 'status_code', 'duration', 'exception')
+    readonly_fields = ('timestamp', 'ip_address', 'user', 'method', 'path', 'status_code', 'duration', 'get_request', 'exception')
+    exclude = ('request', )
+    date_hierarchy = 'timestamp'
+
+    def has_add_permission(self, request, **kwargs):
+        return False
+
+    def no_exception(self, obj):
+        return not bool(obj.exception)
+    no_exception.boolean = True
+
+    def get_request(self, instance):
+        return format_html('<pre>{request}</pre>', request=instance.request)
+    get_request.short_description = 'Request'
+
+admin.site.register(RequestLog, RequestLogAdmin)
---
+++ src/logs/middleware.py
@@ -0,0 +1,79 @@
+import logging
+import time
+import traceback
+
+from logs.models import RequestLog
+
+
+logger = logging.getLogger(__name__)
+
+
+class LogRequestsMiddleware(object):
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        request._start_time = time.monotonic_ns()
+        request_body = self.request_body(request)
+        response = self.get_response(request)
+        if response.streaming:
+            return response
+        if not self.log_filter(request):
+            return response
+        try:
+            RequestLog.objects.create(
+                ip_address=self.client_ip(request),
+                user=self.user_email(request),
+                method=request.method,
+                path=request.path,
+                request=request_body,
+                status_code=response.status_code,
+                exception=getattr(request, '_captured_exception', None) or None,
+                duration=(time.monotonic_ns() - request._start_time) / 1000000000.0,
+            )
+        except:
+            logger.exception("Failed to create APILog record")
+        return response
+
+    def process_exception(self, request, exception):
+        request._captured_exception = str(exception) + '\n\n' + traceback.format_exc()
+        return None
+
+    def log_filter(self, request):
+        if request.path.count('/admin/') or request.path.count('_nested_admin'):
+            return False
+        if request.path == '/favicon.ico':
+            return False
+        return True
+
+    def user_email(self, request):
+        u = getattr(request, 'user', '')
+        if u:
+            if getattr(u, 'email', ''):
+                return u.email
+        return u
+
+    def client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+
+    def request_body(self, request):
+        raw_request_body = ""
+        if request.POST:
+            try:
+                raw_request_body += '\n'.join(['%s=%s' % (k, v) for k, v in request.POST.items() if k != 'csrfmiddlewaretoken'])
+            except Exception as e:
+                raw_request_body += str(e)
+        if request.GET:
+            if raw_request_body:
+                raw_request_body += '\n'
+            try:
+                raw_request_body += '\n'.join(['%s=%s' % (k, v) for k, v in request.GET.items()])
+            except Exception as e:
+                raw_request_body += str(e)
+        return raw_request_body

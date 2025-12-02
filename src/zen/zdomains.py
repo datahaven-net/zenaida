03cf01e71eaf6718fdee3a1297fc09a89fb46fe0
Veselin Penev <penev.veselin@gmail.com>
2025-12-02 18:04:17 +0100

    bug fix in zdomains.domain_update_statuses()

--- src/zen/zdomains.py
+++ src/zen/zdomains.py
@@ -641,6 +641,8 @@ def domain_update_statuses(domain_object, domain_info_response, save=True):
     for st in rgp_status:
         if '@s' in st:
             new_domain_extensions[str(st['@s'])] = ''
+        else:
+            new_domain_extensions.update(st)
     modified = (sorted(current_domain_statuses.keys()) != sorted(new_domain_statuses.keys()))
     extensions_modified = (sorted(current_domain_extensions.keys()) != sorted(new_domain_extensions.keys()))
     updated = False
